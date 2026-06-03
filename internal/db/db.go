7ad5ce77610c25129c7d68111ee1e9c0bd7df02e
Christian Grobmeier <cg@grobmeier.de>
2026-06-03 15:50:24 +0200

    Handle SQLite busy database errors

--- internal/db/db.go
+++ internal/db/db.go
@@ -9,18 +9,19 @@ import (
 	_ "modernc.org/sqlite"
 )
 
+const busyTimeoutMillis = 5000
+
 func Open(path string) (*sql.DB, error) {
 	if err := os.MkdirAll(filepath.Dir(path), 0o755); err != nil {
 		return nil, err
 	}
-	db, err := sql.Open("sqlite", fmt.Sprintf("file:%s?_pragma=foreign_keys(1)", path))
+	db, err := sql.Open("sqlite", fmt.Sprintf("file:%s?_pragma=foreign_keys(1)&_pragma=busy_timeout(%d)", path, busyTimeoutMillis))
 	if err != nil {
-		return nil, err
+		return nil, WrapBusyError(path, err)
 	}
 	if err := db.Ping(); err != nil {
 		_ = db.Close()
-		return nil, err
+		return nil, WrapBusyError(path, err)
 	}
 	return db, nil
 }
-
